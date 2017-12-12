978a83175f1810b85ec0b89b544d7edd8ef96f50
staho <stacho688@gmail.com>
2017-12-12 21:45:32 +0100

    Added grid and training

--- PSI_5/Grid.py
+++ PSI_5/Grid.py
@@ -1,10 +1,25 @@
-import Neuron
+import NeuronKohonen as nk
 
 """Class for Kohonen network""""
 class Grid:
-    def __init__(self, noOfInputs, learningRate, height, width, activationFunction):
+    def __init__(self, noOfInputs, learningRate, height, width):
         self.__dict__['_noOfInputs'] = noOfInputs
         self.__dict__['_learningRate'] = learningRate
         self.__dict__['_height'] = height
         self.__dict__['_width'] = width
-        self.__dict__['_activationFunction'] = activationFunction
+        self.__dict__['_neurons'] = [[NeuronKohonen(self._learningRate, self._noOfInputs) for x in range(self._width)] for y in range(self._height) ]
+
+    def train(self, inputs):
+        highestOutput = 0
+        winner = (0,0)
+
+        for i in range(self._height):
+            for j in range(self._width):
+                
+                tmp = self._neurons[i][j].guess(inputs)
+                if tmp > highestOutput:
+                    highestOutput = tmp
+                    winner = (i, j)
+        
+        self._neurons[winner[0]][winner[j]].train()
+    
--- PSI_5/main.py
+++ PSI_5/main.py
@@ -1,3 +1,8 @@
+import Grid
 
-if __name__ == __main__:
+
+if __name__ == '__main__':
+    learningRate = 0.01
+    noOfInputs = 4
     
+    grid = Grid(learningRate, noOfInputs)
